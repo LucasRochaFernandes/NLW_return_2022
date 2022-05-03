347736fb008a6166df1c6c35542faf6789c73194
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2022-05-03 00:10:57 -0300

    css in className

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
  
 
 function App() {
-  return <button className="bg-violet-500 p-2">butão</button>;
+  return <button className="buttonExample">butão</button>;
 }
 
 export default App;
