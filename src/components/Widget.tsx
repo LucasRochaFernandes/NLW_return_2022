98e22d988caabf91120a031e340257f137c2c7d0
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2022-05-04 00:28:47 -0300

    useState

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -4,19 +4,20 @@ import { useState } from "react";
 export function Widget() {
   const [isWidgetOpen, setIsWidgetOpen] = useState<boolean>(false);
 
-  function active(): void {
-    setIsWidgetOpen(true);
+  function toggleWidgetVisibility(): void {
+    setIsWidgetOpen(!isWidgetOpen);
   }
 
   return (
     <div className="absolute bottom-5 right-5">
-      <h1 className={isWidgetOpen ? "text-red-600" : "text-white"}>OIOI</h1>
+      {isWidgetOpen ? <p>Hello</p> : null}
       <button
         className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"
-        onClick={() => active()}
+        onClick={() => toggleWidgetVisibility()}
       >
         <ChatTeardropDots className="w-6 h-6" />
-        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs  transition-all duration-500 ease-linear">
+          <span className="ml-2"></span>
           FeedBack
         </span>
       </button>
